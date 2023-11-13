f34344f4da9a1ed03330667a825841253c7ca9eb
Jennifer <jennifertan1994@gmail.com>
2023-11-13 15:54:21 -0500

    start

---
+++ src/components/AdDesigner.tsx
@@ -0,0 +1,11 @@
+import './AdDesigner'
+
+const AdDesigner = () => {
+  return (
+    <div className='AdDesigner'>
+        AdDesigner works
+    </div>
+  )
+};
+
+export default AdDesigner;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='Header'>
+        Header works
+    </div>
+  )
+};
+
+export default Header;
+
